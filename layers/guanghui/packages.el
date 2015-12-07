31d41988e2415a25a35381873464c32f01d12b64
zilongshanren <guanghui8827@126.com>
2015-12-07 09:50:27 +0800

    wrap org in with-eval-after-load

--- layers/guanghui/packages.el
+++ layers/guanghui/packages.el
@@ -589,159 +589,156 @@
   (evil-leader/set-key "pf" 'zilongshanren/open-file-with-projectile-or-lsgit))
 
 (defun guanghui/post-init-org ()
-  ;; define the refile targets
-  (setq org-agenda-files (quote ("~/org-notes" )))
-  (setq org-default-notes-file "~/org-notes/gtd.org")
+  (with-eval-after-load 'org
+    (progn
+      ;; define the refile targets
+      (setq org-agenda-files (quote ("~/org-notes" )))
+      (setq org-default-notes-file "~/org-notes/gtd.org")
 
-  ;; the %i would copy the selected text into the template
-  ;;http://www.howardism.org/Technical/Emacs/journaling-org.html
-  ;;add multi-file journal
-  (setq org-capture-templates
-        '(("t" "Todo" entry (file+headline "~/org-notes/gtd.org" "Workspace")
-           "* TODO %?\n  %i\n"
-           :empty-lines 1)
-          ("n" "notes" entry (file+headline "~/org-notes/notes.org" "Quick notes")
-           "* TODO [#C] %?\n  %i\n %U"
-           :empty-lines 1)
-          ("b" "Blog Ideas" entry (file+headline "~/org-notes/notes.org" "Blog Ideas")
-           "* TODO %?\n  %i\n %U"
-           :empty-lines 1)
-          ("w" "work" entry (file+headline "~/org-notes/gtd.org" "Cocos2D-X")
-           "* TODO %?\n  %i\n %U"
-           :empty-lines 1)
-          ("c" "Chrome" entry (file+headline "~/org-notes/notes.org" "Quick notes")
-           "* TODO %?\n %(zilongshanren/retrieve-chrome-current-tab-url)\n %i\n %U"
-           :empty-lines 1)
-          ("l" "links" entry (file+headline "~/org-notes/notes.org" "Quick notes")
-           "* TODO %?\n  %i\n %a \n %U"
-           :empty-lines 1)
-          ("j" "Journal Entry"
-           entry (file+datetree "~/org-notes/journal.org")
-           "* %?"
-           :empty-lines 1)))
+      ;; the %i would copy the selected text into the template
+      ;;http://www.howardism.org/Technical/Emacs/journaling-org.html
+      ;;add multi-file journal
+      (setq org-capture-templates
+            '(("t" "Todo" entry (file+headline "~/org-notes/gtd.org" "Workspace")
+               "* TODO %?\n  %i\n"
+               :empty-lines 1)
+              ("n" "notes" entry (file+headline "~/org-notes/notes.org" "Quick notes")
+               "* TODO [#C] %?\n  %i\n %U"
+               :empty-lines 1)
+              ("b" "Blog Ideas" entry (file+headline "~/org-notes/notes.org" "Blog Ideas")
+               "* TODO %?\n  %i\n %U"
+               :empty-lines 1)
+              ("w" "work" entry (file+headline "~/org-notes/gtd.org" "Cocos2D-X")
+               "* TODO %?\n  %i\n %U"
+               :empty-lines 1)
+              ("c" "Chrome" entry (file+headline "~/org-notes/notes.org" "Quick notes")
+               "* TODO %?\n %(zilongshanren/retrieve-chrome-current-tab-url)\n %i\n %U"
+               :empty-lines 1)
+              ("l" "links" entry (file+headline "~/org-notes/notes.org" "Quick notes")
+               "* TODO %?\n  %i\n %a \n %U"
+               :empty-lines 1)
+              ("j" "Journal Entry"
+               entry (file+datetree "~/org-notes/journal.org")
+               "* %?"
+               :empty-lines 1)))
 
-  ;;An entry without a cookie is treated just like priority ' B '.
-  ;;So when create new task, they are default 重要且紧急
-  (setq org-agenda-custom-commands
-        '(
-          ("w" . "任务安排")
-          ("wa" "重要且紧急的任务" tags-todo "+PRIORITY=\"A\"")
-          ("wb" "重要且不紧急的任务" tags-todo "-Weekly-Monthly-Daily+PRIORITY=\"B\"")
-          ("wc" "不重要且紧急的任务" tags-todo "+PRIORITY=\"C\"")
-          ("b" "Blog" tags-todo "BLOG")
-          ("p" . "项目安排")
-          ("pw" tags-todo "PROJECT+WORK+CATEGORY=\"cocos2d-x\"")
-          ("pl" tags-todo "PROJECT+DREAM+CATEGORY=\"zilongshanren\"")
-          ("W" "Weekly Review"
-           ((stuck "")            ;; review stuck projects as designated by org-stuck-projects
-            (tags-todo "PROJECT") ;; review all projects (assuming you use todo keywords to designate projects)
-            ))))
+      ;;An entry without a cookie is treated just like priority ' B '.
+      ;;So when create new task, they are default 重要且紧急
+      (setq org-agenda-custom-commands
+            '(
+              ("w" . "任务安排")
+              ("wa" "重要且紧急的任务" tags-todo "+PRIORITY=\"A\"")
+              ("wb" "重要且不紧急的任务" tags-todo "-Weekly-Monthly-Daily+PRIORITY=\"B\"")
+              ("wc" "不重要且紧急的任务" tags-todo "+PRIORITY=\"C\"")
+              ("b" "Blog" tags-todo "BLOG")
+              ("p" . "项目安排")
+              ("pw" tags-todo "PROJECT+WORK+CATEGORY=\"cocos2d-x\"")
+              ("pl" tags-todo "PROJECT+DREAM+CATEGORY=\"zilongshanren\"")
+              ("W" "Weekly Review"
+               ((stuck "")            ;; review stuck projects as designated by org-stuck-projects
+                (tags-todo "PROJECT") ;; review all projects (assuming you use todo keywords to designate projects)
+                ))))
 
-  (defvar zilongshanren-website-html-preamble
-    "<div class='nav'>
+      (defvar zilongshanren-website-html-preamble
+        "<div class='nav'>
 <ul>
 <li><a href='http://zilongshanren.com'>博客</a></li>
 <li><a href='/index.html'>Wiki目录</a></li>
 </ul>
 </div>")
-  (defvar zilongshanren-website-html-blog-head
-    " <link rel='stylesheet' href='css/site.css' type='text/css'/> \n
+      (defvar zilongshanren-website-html-blog-head
+        " <link rel='stylesheet' href='css/site.css' type='text/css'/> \n
     <link rel=\"stylesheet\" type=\"text/css\" href=\"/css/worg.css\"/>")
-  (setq org-publish-project-alist
-        `(
-          ("blog-notes"
-           :base-directory "~/org-notes"
-           :base-extension "org"
-           :publishing-directory "~/org-notes/public_html/"
+      (setq org-publish-project-alist
+            `(
+              ("blog-notes"
+               :base-directory "~/org-notes"
+               :base-extension "org"
+               :publishing-directory "~/org-notes/public_html/"
 
-           :recursive t
-           :html-head , zilongshanren-website-html-blog-head
-           :publishing-function org-html-publish-to-html
-           :headline-levels 4           ; Just the default for this project.
-           :auto-preamble t
-           :exclude "gtd.org"
-           :exclude-tags ("ol" "noexport")
-           :section-numbers nil
-           :html-preamble ,zilongshanren-website-html-preamble
-           :author "zilongshanren"
-           :email "guanghui8827@gmail.com"
-           :auto-sitemap t               ; Generate sitemap.org automagically...
-           :sitemap-filename "index.org" ; ... call it sitemap.org (it's the default)...
-           :sitemap-title "我的wiki"     ; ... with title 'Sitemap'.
-           :sitemap-sort-files anti-chronologically
-           :sitemap-file-entry-format "%t" ; %d to output date, we don't need date here
-           )
-          ("blog-static"
-           :base-directory "~/org-notes"
-           :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-           :publishing-directory "~/org-notes/public_html/"
-           :recursive t
-           :publishing-function org-publish-attachment
-           )
-          ("blog" :components ("blog-notes" "blog-static"))))
+               :recursive t
+               :html-head , zilongshanren-website-html-blog-head
+               :publishing-function org-html-publish-to-html
+               :headline-levels 4           ; Just the default for this project.
+               :auto-preamble t
+               :exclude "gtd.org"
+               :exclude-tags ("ol" "noexport")
+               :section-numbers nil
+               :html-preamble ,zilongshanren-website-html-preamble
+               :author "zilongshanren"
+               :email "guanghui8827@gmail.com"
+               :auto-sitemap t               ; Generate sitemap.org automagically...
+               :sitemap-filename "index.org" ; ... call it sitemap.org (it's the default)...
+               :sitemap-title "我的wiki"     ; ... with title 'Sitemap'.
+               :sitemap-sort-files anti-chronologically
+               :sitemap-file-entry-format "%t" ; %d to output date, we don't need date here
+               )
+              ("blog-static"
+               :base-directory "~/org-notes"
+               :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+               :publishing-directory "~/org-notes/public_html/"
+               :recursive t
+               :publishing-function org-publish-attachment
+               )
+              ("blog" :components ("blog-notes" "blog-static"))))
 
-  (defun org-summary-todo (n-done n-not-done)
-    "Switch entry to DONE when all subentries are done, to TODO otherwise."
-    (let (org-log-done org-log-states)  ; turn off logging
-      (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+      (defun org-summary-todo (n-done n-not-done)
+        "Switch entry to DONE when all subentries are done, to TODO otherwise."
+        (let (org-log-done org-log-states)  ; turn off logging
+          (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
 
-  (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
-  ;; used by org-clock-sum-today-by-tags
-  (defun filter-by-tags ()
-    (let ((head-tags (org-get-tags-at)))
-      (member current-tag head-tags)))
+      (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+      ;; used by org-clock-sum-today-by-tags
+      (defun filter-by-tags ()
+        (let ((head-tags (org-get-tags-at)))
+          (member current-tag head-tags)))
 
-  (defun org-clock-sum-today-by-tags (timerange &optional tstart tend noinsert)
-    (interactive "P")
-    (let* ((timerange-numeric-value (prefix-numeric-value timerange))
-           (files (org-add-archive-files (org-agenda-files)))
-           (include-tags '("WORK" "EMACS" "DREAM" "WRITING" "MEETING"
-                           "LIFE" "PROJECT" "OTHER"))
-           (tags-time-alist (mapcar (lambda (tag) `(,tag . 0)) include-tags))
-           (output-string "")
-           (tstart (or tstart
-                       (and timerange (equal timerange-numeric-value 4) (- (org-time-today) 86400))
-                       (and timerange (equal timerange-numeric-value 16) (org-read-date nil nil nil "Start Date/Time:"))
-                       (org-time-today)))
-           (tend (or tend
-                     (and timerange (equal timerange-numeric-value 16) (org-read-date nil nil nil "End Date/Time:"))
-                     (+ tstart 86400)))
-           h m file item prompt donesomething)
-      (while (setq file (pop files))
-        (setq org-agenda-buffer (if (file-exists-p file)
-                                    (org-get-agenda-file-buffer file)
-                                  (error "No such file %s" file)))
-        (with-current-buffer org-agenda-buffer
-          (dolist (current-tag include-tags)
-            (org-clock-sum tstart tend 'filter-by-tags)
-            (setcdr (assoc current-tag tags-time-alist)
-                    (+ org-clock-file-total-minutes (cdr (assoc current-tag tags-time-alist)))))))
-      (while (setq item (pop tags-time-alist))
-        (unless (equal (cdr item) 0)
-          (setq donesomething t)
-          (setq h (/ (cdr item) 60)
-                m (- (cdr item) (* 60 h)))
-          (setq output-string (concat output-string (format "[-%s-] %.2d:%.2d\n" (car item) h m)))))
-      (unless donesomething
-        (setq output-string (concat output-string "[-Nothing-] Done nothing!!!\n")))
-      (unless noinsert
-        (insert output-string))
-      output-string))
-
-  (eval-after-load 'org
-    '(progn
-       (global-set-key (kbd "C-c a") 'org-agenda)
-       (define-key org-mode-map (kbd "s-p") 'org-priority)
-       (define-key global-map (kbd "<f9>") 'org-capture)
-       (global-set-key (kbd "C-c b") 'org-iswitchb)
-       (define-key evil-normal-state-map (kbd "C-c C-w") 'org-refile)
-       (evil-leader/set-key-for-mode 'org-mode
-         "owh" 'plain-org-wiki-helm
-         "owf" 'plain-org-wiki)
-       ))
-
-  (setq org-mobile-directory "~/org-notes/org")
-  )
+      (defun org-clock-sum-today-by-tags (timerange &optional tstart tend noinsert)
+        (interactive "P")
+        (let* ((timerange-numeric-value (prefix-numeric-value timerange))
+               (files (org-add-archive-files (org-agenda-files)))
+               (include-tags '("WORK" "EMACS" "DREAM" "WRITING" "MEETING"
+                               "LIFE" "PROJECT" "OTHER"))
+               (tags-time-alist (mapcar (lambda (tag) `(,tag . 0)) include-tags))
+               (output-string "")
+               (tstart (or tstart
+                           (and timerange (equal timerange-numeric-value 4) (- (org-time-today) 86400))
+                           (and timerange (equal timerange-numeric-value 16) (org-read-date nil nil nil "Start Date/Time:"))
+                           (org-time-today)))
+               (tend (or tend
+                         (and timerange (equal timerange-numeric-value 16) (org-read-date nil nil nil "End Date/Time:"))
+                         (+ tstart 86400)))
+               h m file item prompt donesomething)
+          (while (setq file (pop files))
+            (setq org-agenda-buffer (if (file-exists-p file)
+                                        (org-get-agenda-file-buffer file)
+                                      (error "No such file %s" file)))
+            (with-current-buffer org-agenda-buffer
+              (dolist (current-tag include-tags)
+                (org-clock-sum tstart tend 'filter-by-tags)
+                (setcdr (assoc current-tag tags-time-alist)
+                        (+ org-clock-file-total-minutes (cdr (assoc current-tag tags-time-alist)))))))
+          (while (setq item (pop tags-time-alist))
+            (unless (equal (cdr item) 0)
+              (setq donesomething t)
+              (setq h (/ (cdr item) 60)
+                    m (- (cdr item) (* 60 h)))
+              (setq output-string (concat output-string (format "[-%s-] %.2d:%.2d\n" (car item) h m)))))
+          (unless donesomething
+            (setq output-string (concat output-string "[-Nothing-] Done nothing!!!\n")))
+          (unless noinsert
+            (insert output-string))
+          output-string))
+      (global-set-key (kbd "C-c a") 'org-agenda)
+      (define-key org-mode-map (kbd "s-p") 'org-priority)
+      (define-key global-map (kbd "<f9>") 'org-capture)
+      (global-set-key (kbd "C-c b") 'org-iswitchb)
+      (define-key evil-normal-state-map (kbd "C-c C-w") 'org-refile)
+      (evil-leader/set-key-for-mode 'org-mode
+        "owh" 'plain-org-wiki-helm
+        "owf" 'plain-org-wiki)
+      (setq org-mobile-directory "~/org-notes/org")
+      )))
 
 (defun guanghui/post-init-prodigy ()
   (prodigy-define-tag
--- layers/zilongshanren/packages.el
+++ layers/zilongshanren/packages.el
@@ -798,100 +798,101 @@ If `F.~REV~' already exists, use it instead of checking it out again."
 ;; http://freizl.github.io/posts/2012-04-06-export-orgmode-file-in-Chinese.html
 ;;http://stackoverflow.com/questions/21005885/export-org-mode-code-block-and-result-with-different-styles
 (defun zilongshanren/post-init-org ()
-  (progn
-    ;; https://github.com/syl20bnr/spacemacs/issues/2994#issuecomment-139737911
-    ;; (when (configuration-layer/package-usedp 'company)
-    ;;   (spacemacs|add-company-hook org-mode))
-    (spacemacs|disable-company org-mode)
-    (evil-leader/set-key-for-mode 'org-mode
-      "," 'org-priority)
-    (require 'org-compat)
-    (require 'org)
-    ;; (add-to-list 'org-modules "org-habit")
-    (add-to-list 'org-modules 'org-habit)
-    (require 'org-habit)
+  (with-eval-after-load 'org
+    (progn
+      ;; https://github.com/syl20bnr/spacemacs/issues/2994#issuecomment-139737911
+      ;; (when (configuration-layer/package-usedp 'company)
+      ;;   (spacemacs|add-company-hook org-mode))
+      (spacemacs|disable-company org-mode)
+      (evil-leader/set-key-for-mode 'org-mode
+        "," 'org-priority)
+      (require 'org-compat)
+      (require 'org)
+      ;; (add-to-list 'org-modules "org-habit")
+      (add-to-list 'org-modules 'org-habit)
+      (require 'org-habit)
 
-    (setq org-refile-use-outline-path 'file)
-    (setq org-outline-path-complete-in-steps nil)
-    (setq org-refile-targets
-          '((nil :maxlevel . 4)
-            (org-agenda-files :maxlevel . 4)))
-    ;; config stuck project
-    (setq org-stuck-projects
-          '("TODO={.+}/-DONE" nil nil "SCHEDULED:\\|DEADLINE:"))
+      (setq org-refile-use-outline-path 'file)
+      (setq org-outline-path-complete-in-steps nil)
+      (setq org-refile-targets
+            '((nil :maxlevel . 4)
+              (org-agenda-files :maxlevel . 4)))
+      ;; config stuck project
+      (setq org-stuck-projects
+            '("TODO={.+}/-DONE" nil nil "SCHEDULED:\\|DEADLINE:"))
 
-    (setq org-agenda-inhibit-startup t)       ;; ~50x speedup
-    (setq org-agenda-use-tag-inheritance nil) ;; 3-4x speedup
-    (setq org-agenda-window-setup 'current-window)
-    (setq org-log-done t)
+      (setq org-agenda-inhibit-startup t)     ;; ~50x speedup
+      (setq org-agenda-use-tag-inheritance nil) ;; 3-4x speedup
+      (setq org-agenda-window-setup 'current-window)
+      (setq org-log-done t)
 
-    ;; 加密文章
-    ;; "http://coldnew.github.io/blog/2013/07/13_5b094.html"
-    ;; org-mode 設定
-    (require 'org-crypt)
+      ;; 加密文章
+      ;; "http://coldnew.github.io/blog/2013/07/13_5b094.html"
+      ;; org-mode 設定
+      (require 'org-crypt)
 
-    ;; 當被加密的部份要存入硬碟時，自動加密回去
-    (org-crypt-use-before-save-magic)
+      ;; 當被加密的部份要存入硬碟時，自動加密回去
+      (org-crypt-use-before-save-magic)
 
-    ;; 設定要加密的 tag 標籤為 secret
-    (setq org-crypt-tag-matcher "secret")
+      ;; 設定要加密的 tag 標籤為 secret
+      (setq org-crypt-tag-matcher "secret")
 
-    ;; 避免 secret 這個 tag 被子項目繼承 造成重複加密
-    ;; (但是子項目還是會被加密喔)
-    (setq org-tags-exclude-from-inheritance (quote ("secret")))
+      ;; 避免 secret 這個 tag 被子項目繼承 造成重複加密
+      ;; (但是子項目還是會被加密喔)
+      (setq org-tags-exclude-from-inheritance (quote ("secret")))
 
-    ;; 用於加密的 GPG 金鑰
-    ;; 可以設定任何 ID 或是設成 nil 來使用對稱式加密 (symmetric encryption)
-    (setq org-crypt-key nil)
+      ;; 用於加密的 GPG 金鑰
+      ;; 可以設定任何 ID 或是設成 nil 來使用對稱式加密 (symmetric encryption)
+      (setq org-crypt-key nil)
 
-    (add-to-list 'auto-mode-alist '("\\.org\\’" . org-mode))
+      (add-to-list 'auto-mode-alist '("\\.org\\’" . org-mode))
 
 
 
-    (setq org-todo-keywords
-          (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
-                  (sequence "WAITING(w@/!)" "SOMEDAY(S)"  "|" "CANCELLED(c@/!)" "MEETING(m)" "PHONE(p)"))))
+      (setq org-todo-keywords
+            (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
+                    (sequence "WAITING(w@/!)" "SOMEDAY(S)"  "|" "CANCELLED(c@/!)" "MEETING(m)" "PHONE(p)"))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ;; Org clock
+      ;; Org clock
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    ;; Change task state to STARTED when clocking in
-    (setq org-clock-in-switch-to-state "STARTED")
-    ;; Save clock data and notes in the LOGBOOK drawer
-    (setq org-clock-into-drawer t)
-    ;; Removes clocked tasks with 0:00 duration
-    (setq org-clock-out-remove-zero-time-clocks t) ;; Show the clocked-in task - if any - in the header line
+      ;; Change task state to STARTED when clocking in
+      (setq org-clock-in-switch-to-state "STARTED")
+      ;; Save clock data and notes in the LOGBOOK drawer
+      (setq org-clock-into-drawer t)
+      ;; Removes clocked tasks with 0:00 duration
+      (setq org-clock-out-remove-zero-time-clocks t) ;; Show the clocked-in task - if any - in the header line
 
-    (setq org-tags-match-list-sublevels nil)
+      (setq org-tags-match-list-sublevels nil)
 
-    ;; http://wenshanren.org/?p=327
-    ;; change it to helm
-    (defun zilongshanren/org-insert-src-block (src-code-type)
-      "Insert a `SRC-CODE-TYPE' type source code block in org-mode."
-      (interactive
-       (let ((src-code-types
-              '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++" "css"
-                "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
-                "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk" "ditaa"
-                "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
-                "scheme" "sqlite")))
-         (list (ido-completing-read "Source code type: " src-code-types))))
-      (progn
-        (newline-and-indent)
-        (insert (format "#+BEGIN_SRC %s\n" src-code-type))
-        (newline-and-indent)
-        (insert "#+END_SRC\n")
-        (previous-line 2)
-        (org-edit-src-code)))
+      ;; http://wenshanren.org/?p=327
+      ;; change it to helm
+      (defun zilongshanren/org-insert-src-block (src-code-type)
+        "Insert a `SRC-CODE-TYPE' type source code block in org-mode."
+        (interactive
+         (let ((src-code-types
+                '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++" "css"
+                  "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
+                  "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk" "ditaa"
+                  "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
+                  "scheme" "sqlite")))
+           (list (ido-completing-read "Source code type: " src-code-types))))
+        (progn
+          (newline-and-indent)
+          (insert (format "#+BEGIN_SRC %s\n" src-code-type))
+          (newline-and-indent)
+          (insert "#+END_SRC\n")
+          (previous-line 2)
+          (org-edit-src-code)))
 
-    (add-hook 'org-mode-hook '(lambda ()
-                                ;; keybinding for editing source code blocks
-                                ;; keybinding for inserting code blocks
-                                (local-set-key (kbd "C-c i s")
-                                               'zilongshanren/org-insert-src-block)
-                                ))
-    (require 'ox-publish)
-    (add-to-list 'org-latex-classes '("ctexart" "\\documentclass[11pt]{ctexart}
+      (add-hook 'org-mode-hook '(lambda ()
+                                  ;; keybinding for editing source code blocks
+                                  ;; keybinding for inserting code blocks
+                                  (local-set-key (kbd "C-c i s")
+                                                 'zilongshanren/org-insert-src-block)
+                                  ))
+      (require 'ox-publish)
+      (add-to-list 'org-latex-classes '("ctexart" "\\documentclass[11pt]{ctexart}
                                         [NO-DEFAULT-PACKAGES]
                                         \\usepackage[utf8]{inputenc}
                                         \\usepackage[T1]{fontenc}
@@ -931,47 +932,47 @@ If `F.~REV~' already exists, use it instead of checking it out again."
                                         showstringspaces=false
                                         }
                                         "
-                                      ("\\section{%s}" . "\\section*{%s}")
-                                      ("\\subsection{%s}" . "\\subsection*{%s}")
-                                      ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                                      ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                                      ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+                                        ("\\section{%s}" . "\\section*{%s}")
+                                        ("\\subsection{%s}" . "\\subsection*{%s}")
+                                        ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                                        ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                                        ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
-    ;; {{ export org-mode in Chinese into PDF
-    ;; @see http://freizl.github.io/posts/tech/2012-04-06-export-orgmode-file-in-Chinese.html
-    ;; and you need install texlive-xetex on different platforms
-    ;; To install texlive-xetex:
-    ;;    `sudo USE="cjk" emerge texlive-xetex` on Gentoo Linux
-    ;; }}
-    (setq org-latex-default-class "ctexart")
-    (setq org-latex-pdf-process
-          '(
-            "xelatex -interaction nonstopmode -output-directory %o %f"
-            "xelatex -interaction nonstopmode -output-directory %o %f"
-            "xelatex -interaction nonstopmode -output-directory %o %f"
-            "rm -fr %b.out %b.log %b.tex auto"))
+      ;; {{ export org-mode in Chinese into PDF
+      ;; @see http://freizl.github.io/posts/tech/2012-04-06-export-orgmode-file-in-Chinese.html
+      ;; and you need install texlive-xetex on different platforms
+      ;; To install texlive-xetex:
+      ;;    `sudo USE="cjk" emerge texlive-xetex` on Gentoo Linux
+      ;; }}
+      (setq org-latex-default-class "ctexart")
+      (setq org-latex-pdf-process
+            '(
+              "xelatex -interaction nonstopmode -output-directory %o %f"
+              "xelatex -interaction nonstopmode -output-directory %o %f"
+              "xelatex -interaction nonstopmode -output-directory %o %f"
+              "rm -fr %b.out %b.log %b.tex auto"))
 
-    (setq org-latex-listings t)
-    ;; improve org babel
+      (setq org-latex-listings t)
+      ;; improve org babel
 
-    (org-babel-do-load-languages
-     'org-babel-load-languages
-     '( (perl . t)
-        (ruby . t)
-        (sh . t)
-        (js . t)
-        (python . t)
-        (emacs-lisp . t)
-        (plantuml . t)
-        (C . t)
-        (R . t)
-        (ditaa . t)))
+      (org-babel-do-load-languages
+       'org-babel-load-languages
+       '( (perl . t)
+          (ruby . t)
+          (sh . t)
+          (js . t)
+          (python . t)
+          (emacs-lisp . t)
+          (plantuml . t)
+          (C . t)
+          (R . t)
+          (ditaa . t)))
 
-    (setq org-plantuml-jar-path
-          (expand-file-name "~/.spacemacs.d/plantuml.jar"))
-    (setq org-ditaa-jar-path "~/.spacemacs.d/ditaa.jar")
+      (setq org-plantuml-jar-path
+            (expand-file-name "~/.spacemacs.d/plantuml.jar"))
+      (setq org-ditaa-jar-path "~/.spacemacs.d/ditaa.jar")
 
-    ))
+      )))
 
 
 
