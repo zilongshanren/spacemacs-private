3e87d5aa59358c83880b5d5a24d112ab0f8df7e5
zilongshanren <guanghui8827@126.com>
2016-01-05 23:36:37 +0800

    update dotspacemacs

--- init.el
+++ init.el
@@ -142,7 +142,7 @@ values."
    dotspacemacs-elpa-timeout 5
    ;; If non nil then spacemacs will check for updates at startup
    ;; when the current branch is not `develop'. (default t)
-   dotspacemacs-check-for-update nil
+   dotspacemacs-check-for-update t
    ;; One of `vim', `emacs' or `hybrid'. Evil is always enabled but if the
    ;; variable is `emacs' then the `holy-mode' is enabled at startup. `hybrid'
    ;; uses emacs key bindings for vim's insert mode, but otherwise leaves evil
