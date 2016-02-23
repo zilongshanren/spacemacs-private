b1f6f22947f7894a1ec47a70eeca9d13bfe5513f
zilongshanren <guanghui8827@126.com>
2016-02-23 18:08:22 +0800

    trun off smoothing scrolling

--- init.el
+++ init.el
@@ -294,7 +294,7 @@ values."
    ;; If non nil smooth scrolling (native-scrolling) is enabled. Smooth
    ;; scrolling overrides the default behavior of Emacs which recenters point
    ;; when it reaches the top or bottom of the screen. (default t)
-   dotspacemacs-smooth-scrolling t
+   dotspacemacs-smooth-scrolling nil
    ;; If non nil line numbers are turned on in all `prog-mode' and `text-mode'
    ;; derivatives. If set to `relative', also turns on relative line numbers.
    ;; (default nil)
