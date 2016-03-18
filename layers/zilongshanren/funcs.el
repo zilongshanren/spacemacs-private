df7b0c8606b56b8e888acf37598ec93a3b454514
zilongshanren <guanghui8827@126.com>
2016-03-18 13:22:02 +0800

    add remove dos eol

--- layers/zilongshanren/funcs.el
+++ layers/zilongshanren/funcs.el
@@ -437,8 +437,14 @@ With PREFIX, cd to project root."
         ((looking-back "[\[\(\{]" 1) (backward-char) (evil-jump-item))
         (t nil)))
 
-(defun zilongshanren/remove-dos-eol ()
+(defun zilongshanren/hidden-dos-eol ()
   "Do not show ^M in files containing mixed UNIX and DOS line endings."
   (interactive)
   (setq buffer-display-table (make-display-table))
   (aset buffer-display-table ?\^M []))
+
+(defun zilongshanren/remove-dos-eol ()
+  "Replace DOS eolns CR LF with Unix eolns CR"
+  (interactive)
+  (goto-char (point-min))
+  (while (search-forward "\r" nil t) (replace-match "")))
