f3ed3ea7c81599296adb5517a918ca246d074e6e
zilongshanren <guanghui8827@126.com>
2016-03-18 09:45:35 +0800

    remove dos eol

--- layers/zilongshanren/funcs.el
+++ layers/zilongshanren/funcs.el
@@ -437,3 +437,8 @@ With PREFIX, cd to project root."
         ((looking-back "[\[\(\{]" 1) (backward-char) (evil-jump-item))
         (t nil)))
 
+(defun zilongshanren/remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
