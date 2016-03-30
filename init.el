3805b0f68883c5ba980a5dee142a4efaf0352f4f
zilongshanren <guanghui8827@126.com>
2016-03-30 16:22:59 +0800

    add go layer

--- init.el
+++ init.el
@@ -32,6 +32,7 @@ values."
    ;; of a list then all discovered layers will be installed.
    dotspacemacs-configuration-layers
    '(
+     go
      windows-scripts
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
