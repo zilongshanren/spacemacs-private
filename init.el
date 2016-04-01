c0187dadf644531aa249b8b05ae62fcace50fd20
zilongshanren <guanghui8827@126.com>
2016-04-01 10:48:27 +0800

    fix loading from shell

--- init.el
+++ init.el
@@ -369,7 +369,7 @@ layers configuration."
 
   ;;解决org表格里面中英文对齐的问题
   (when (configuration-layer/layer-usedp 'chinese)
-    (when (spacemacs/system-is-mac)
+    (when (and (spacemacs/system-is-mac) window-system)
       (spacemacs//set-monospaced-font "Source Code Pro" "Hiragino Sans GB" 14 16)))
 
   ;; (global-company-mode t)
