4eb51030b27dadabce82810957120fc32b2669ef
zilongshanren <guanghui8827@126.com>
2016-04-06 10:20:30 +0800

    add company for shell-script-mode

--- layers/zilongshanren/config.el
+++ layers/zilongshanren/config.el
@@ -14,7 +14,9 @@
 (spacemacs|defvar-company-backends org-mode)
 (spacemacs|defvar-company-backends nxml-mode)
 (spacemacs|defvar-company-backends sh-mode)
+(spacemacs|defvar-company-backends shell-script-mode)
 (spacemacs|add-company-hook sh-mode)
+(spacemacs|add-company-hook shell-script-mode)
 
 (spacemacs/declare-prefix "ot" "Toggle")
 
