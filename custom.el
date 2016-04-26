b5db9c519aed8cc6e70feb423642beb4a697610b
zilongshanren <guanghui8827@126.com>
2016-04-26 15:45:19 +0800

    fix toggle theme issue

--- custom.el
+++ custom.el
@@ -72,7 +72,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 257)) (:foreground "#F8F8F2" :background "#272822")) (((class color) (min-colors 89)) (:foreground "#F5F5F5" :background "#1B1E1C"))))
  '(company-tooltip-annotation ((t (:inherit company-tooltip :foreground "green"))))
  '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
  '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil))))
