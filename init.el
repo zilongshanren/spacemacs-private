650dd5758c41b6bcbd26a3573767b114368c93c4
zilongshanren <guanghui8827@126.com>
2016-05-11 09:51:00 +0800

    add which-func to mode-line

--- init.el
+++ init.el
@@ -397,12 +397,8 @@ layers configuration."
   (spacemacs/toggle-automatic-symbol-highlight-on)
   ;; http://emacsredux.com/blog/2014/04/05/which-function-mode/
   ;; when editing js file, this feature is very useful
-  ;; (setq-default header-line-format
-  ;;               '((which-func-mode ("" which-func-format " "))))
-  (setq mode-line-misc-info
-        ;; We remove Which Function Mode from the mode line, because it's mostly
-        ;; invisible here anyway.
-        (assq-delete-all 'which-func-mode mode-line-misc-info))
+  (setq-default header-line-format
+                '((which-func-mode ("" which-func-format " "))))
 
   (add-hook 'prog-mode-hook
             (lambda ()
