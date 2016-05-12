4b0583810546a70a1e2dd4dcfa1e43fb0c0fde78
zilongshanren <guanghui8827@126.com>
2016-05-12 22:40:41 +0800

    disable lazy installation

--- init.el
+++ init.el
@@ -20,7 +20,7 @@ values."
    ;; installation feature and you have to explicitly list a layer in the
    ;; variable `dotspacemacs-configuration-layers' to install it.
    ;; (default 'unused)
-   dotspacemacs-enable-lazy-installation 'unused
+   dotspacemacs-enable-lazy-installation 'nil
    ;; If non-nil then Spacemacs will ask for confirmation before installing
    ;; a layer lazily. (default t)
    dotspacemacs-ask-for-lazy-installation t
