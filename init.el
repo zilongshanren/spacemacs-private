ffdcf53bcda493c39309be3ec081b0cbf1c7e577
zilongshanren <guanghui8827@126.com>
2016-06-28 12:06:00 +0800

    improve mode line

--- init.el
+++ init.el
@@ -515,11 +515,87 @@ layers configuration."
                   name)
                 'face 'bold)
                (setq global-mode-string
-                     (concat "      [    " name "    ]"))))))
+                     (list (concat "      [" name "]")))))))
 
   (add-hook 'persp-activated-functions 'zilongshanren/update-persp-name)
 
+  (defun mode-line-fill (face reserve)
+    "Return empty space using FACE and leaving RESERVE space on the right."
+    (unless reserve
+      (setq reserve 20))
+    (when (and window-system (eq 'right (get-scroll-bar-mode)))
+      (setq reserve (- reserve 3)))
+    (propertize " "
+                'display `((space :align-to (- (+ right right-fringe right-margin) ,reserve)))
+                'face face))
 
+  (setq-default mode-line-format
+                (list
+                 ;; the buffer name; the file name as a tool tip
+                 '(:eval (propertize "%b " 'face 'font-lock-keyword-face
+                                     'help-echo (buffer-file-name)))
+
+                 ;; line and column
+                 "(" ;; '%02' to set to 2 chars at least; prevents flickering
+                 (propertize "%02l" 'face 'font-lock-type-face) ","
+                 (propertize "%02c" 'face 'font-lock-type-face)
+                 ") "
+
+                 " [" ;; insert vs overwrite mode, input-method in a tooltip
+                 '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+                                     'face 'font-lock-preprocessor-face
+                                     'help-echo (concat "Buffer is in "
+                                                        (if overwrite-mode "overwrite" "insert") " mode")))
+
+                 ;; was this buffer modified since the last save?
+                 '(:eval (when (buffer-modified-p)
+                           (concat ","  (propertize "Mod"
+                                                    'face 'font-lock-warning-face
+                                                    'help-echo "Buffer has been modified"))))
+
+                 ;; is this buffer read-only?
+                 '(:eval (when buffer-read-only
+                           (concat ","  (propertize "RO"
+                                                    'face 'font-lock-type-face
+                                                    'help-echo "Buffer is read-only"))))
+                 "] "
+
+                 anzu--mode-line-format
+
+
+                 ;; relative position, size of file
+                 "["
+                 (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
+                 "/"
+                 (propertize "%I" 'face 'font-lock-constant-face) ;; size
+                 "] "
+
+                 ;; the current major mode for the buffer.
+                 '(:eval (propertize "%m" 'face 'font-lock-string-face
+                                     'help-echo buffer-file-coding-system))
+
+                 "%2 "
+
+                 evil-mode-line-tag
+
+                 minor-mode-alist
+
+                 `(vc-mode vc-mode)
+
+                 " "
+
+                 ;; flycheck-mode-line
+
+                 mode-line-misc-info
+
+                 (mode-line-fill 'mode-line 20)
+
+                 ;; add the time, with the date and the emacs uptime in the tooltip
+                 ;; '(:eval (propertize (format-time-string "%H:%M")
+                 ;;                     'help-echo
+                 ;;                     (concat (format-time-string "%c; ")
+                 ;;                             (emacs-uptime "Uptime:%hh"))))
+                 ))
 
   )
 
