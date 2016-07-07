9648fac3e6027fb0209ed0f8c7062779d99b9c93
zilongshanren <guanghui8827@126.com>
2016-07-07 10:09:45 +0800

    helm-locate is very useful

--- layers/guanghui/packages.el
+++ layers/guanghui/packages.el
@@ -59,7 +59,21 @@
         flycheck-clojure
         paredit
         youdao-dictionary
+        helm
         ))
+
+(defun guanghui/post-init-helm ()
+  (with-eval-after-load 'helm
+    (progn
+      ;; limit max number of matches displayed for speed
+      (setq helm-candidate-number-limit 100)
+      ;; ignore boring files like .o and .a
+      (setq helm-ff-skip-boring-files t)
+      ;; replace locate with spotlight on Mac
+      (setq helm-locate-command "mdfind -name %s %s")
+      (push "\\.emlx$" helm-boring-file-regexp-list)
+      )
+    ))
 
 (defun guanghui/init-youdao-dictionary ()
   (use-package youdao-dictionary
