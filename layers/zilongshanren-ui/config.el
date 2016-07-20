78708bdd4b832e0e319f8371321593769ef03678
zilongshanren <guanghui8827@126.com>
2016-07-20 09:29:07 +0800

    add zilongshanren-ui layer

---
+++ layers/zilongshanren-ui/config.el
@@ -0,0 +1,14 @@
+;;; config.el --- zilongshanren Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2015-2016 zilongshanren
+;;
+;; Author: zilongshanren <guanghui8827@gmail.com>
+;; URL: https://github.com/zilongshanren/spacemacs-private
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; for fixing powerline separator issue
+;; (setq-default ns-use-srgb-colorspace nil)
+
