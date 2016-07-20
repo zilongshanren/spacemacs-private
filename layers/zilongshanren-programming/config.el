2d66d94565458638b3af063198ff460eb75320cf
zilongshanren <guanghui8827@126.com>
2016-07-20 13:22:00 +0800

    refine configs

--- layers/zilongshanren-programming/config.el
+++ layers/zilongshanren-programming/config.el
@@ -149,14 +149,6 @@ Single Capitals as you type."
 
 (setq url-show-status nil)
 
-(defun endless/org-ispell ()
-  "Configure `ispell-skip-region-alist' for `org-mode'."
-  (make-local-variable 'ispell-skip-region-alist)
-  (add-to-list 'ispell-skip-region-alist '(org-property-drawer-re))
-  (add-to-list 'ispell-skip-region-alist '("~" "~"))
-  (add-to-list 'ispell-skip-region-alist '("=" "="))
-  (add-to-list 'ispell-skip-region-alist '("^#\\+BEGIN_SRC" . "^#\\+END_SRC")))
-(add-hook 'org-mode-hook #'endless/org-ispell)
 
 
 ;;Don’t ask me when close emacs with process is running
