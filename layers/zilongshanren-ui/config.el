c0c4e29a718ed2497987996fb1784c3e444e1e6e
Pro Wang <prowanghao@gmail.com>
2016-07-26 07:19:14 +0800

    modify UI Title to WangHao

--- layers/zilongshanren-ui/config.el
+++ layers/zilongshanren-ui/config.el
@@ -21,7 +21,7 @@
 ;; more useful frame title, that show either a file or a
 ;; buffer name (if the buffer isn't visiting a file)
 (setq frame-title-format
-      '("" " Guanghui - "
+      '("" " WangHao - "
         (:eval (if (buffer-file-name)
                    (abbreviate-file-name (buffer-file-name)) "%b"))))
 
