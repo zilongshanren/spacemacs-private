fc45d63668195e61e453b3707697147d13e76c64
Wang Hao <wanghaohs@hangsheng.com.cn>
2016-08-01 17:32:07 +0800

    fix chinese font setting

--- init.el
+++ init.el
@@ -157,7 +157,7 @@
   ;;解决org表格里面中英文对齐的问题
   (when (configuration-layer/layer-usedp 'chinese)
     (when (and (spacemacs/system-is-mac) window-system)
-      (spacemacs//set-monospaced-font "Source Code Pro" "Hiragino Sans GB" 14 16)))
+      (spacemacs//set-monospaced-font "Source Code Pro" "AR PL UMing CN" 14 16)))
 
 
   (spacemacs|add-company-hook 'text-mode)
