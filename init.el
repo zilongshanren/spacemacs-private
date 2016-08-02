75356d1bb305866205ef725248558d898dbfbb3e
Pro Wang <prowanghao@gmail.com>
2016-08-02 12:09:00 +0800

    去除原来的org-table对齐配置

--- init.el
+++ init.el
@@ -155,9 +155,9 @@
 
 (defun dotspacemacs/user-config ()
   ;;解决org表格里面中英文对齐的问题
-  (when (configuration-layer/layer-usedp 'chinese)
-    (when (and (spacemacs/system-is-mac) window-system)
-      (spacemacs//set-monospaced-font "Source Code Pro" "AR PL UMing CN" 14 16)))
+  ;; (when (configuration-layer/layer-usedp 'chinese)
+  ;;   (when (and (spacemacs/system-is-mac) window-system)
+  ;;     (spacemacs//set-monospaced-font "Source Code Pro" "AR PL UMing CN" 14 16)))
 
 
   (spacemacs|add-company-hook 'text-mode)
