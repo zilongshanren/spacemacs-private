0b75f87b4d19b3499c6240d790b24a713c274b81
zilongshanren <guanghui8827@126.com>
2016-09-07 15:20:05 +0800

    add helm-projectile back

--- init.el
+++ init.el
@@ -74,7 +74,7 @@
                         clang-format define-word google-translate disaster epic
                         fancy-battery neotree org-present orgit orglue spacemacs-theme
                         helm-flyspell flyspell-correct-helm clean-aindent-mode
-                        helm-c-yasnippet ace-jump-helm-line helm-make helm-projectile
+                        helm-c-yasnippet ace-jump-helm-line helm-make
                         helm-themes helm-swoop helm-spacemacs-help smeargle
                         ido-vertical-mode flx-ido company-quickhelp)
    dotspacemacs-install-packages 'used-only
