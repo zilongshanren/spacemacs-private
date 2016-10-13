2eb29ad82a0c1f0b24d418778360518f4ef7c46f
zilongshanren <guanghui8827@126.com>
2016-10-13 09:53:51 +0800

    add toggle major mode

--- init.el
+++ init.el
@@ -259,6 +259,13 @@
         (when (looking-at (concat "^" (make-string tab-width ?\ )))
           (replace-match "")))))
 
+  (defun zilongshanren/toggle-major-mode ()
+    (interactive)
+    (if (eq major-mode 'fundamental-mode)
+        (set-auto-mode)
+      (fundamental-mode)))
+  (spacemacs/set-leader-keys "otm" 'zilongshanren/toggle-major-mode)
+
   (add-hook 'text-mode-hook 'spacemacs/toggle-spelling-checking-on))
 
 (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
