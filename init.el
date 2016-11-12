6a8599ddb6fdf0070e2ca1f176e0386afc969454
zilongshanren <guanghui8827@126.com>
2016-11-12 18:30:44 +0800

    update dot files

--- init.el
+++ init.el
@@ -334,7 +334,7 @@ values."
    ;; `trailing' to delete only the whitespace at end of lines, `changed'to
    ;; delete only whitespace for changed lines or `nil' to disable cleanup.
    ;; (default nil)
-   dotspacemacs-whitespace-cleanup nil
+   dotspacemacs-whitespace-cleanup 'changed
    ))
 
 (defun dotspacemacs/user-init ()
