1cead58255051cf32923376b5aea798775438455
zilongshanren <guanghui8827@126.com>
2017-02-07 19:57:58 +0800

    use spacemacs light theme

--- init.el
+++ init.el
@@ -174,7 +174,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
+   dotspacemacs-themes '(spacemacs-light spacemacs-dark
                          solarized-light
                          solarized-dark)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
