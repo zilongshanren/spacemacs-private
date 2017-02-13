68293278d0e0cb936205728016e153f0a8241815
zilongshanren <guanghui8827@126.com>
2017-02-13 14:45:37 +0800

    omit meta file in dired mode

--- layers/zilongshanren-better-defaults/packages.el
+++ layers/zilongshanren-better-defaults/packages.el
@@ -63,7 +63,7 @@
               ("\\.md\\'" "open")))
 
       (setq dired-omit-files
-      (concat dired-omit-files "\\|^.DS_STORE$\\|^.projectile$"))
+      (concat dired-omit-files "\\|^.DS_Store$\\|^.projectile$\\|\\.js\\.meta$"))
 
       ;; always delete and copy recursively
       (setq dired-recursive-deletes 'always)
