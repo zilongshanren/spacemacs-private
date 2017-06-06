4d24cc9c15e4b4dd776de3496cea2f303c4e5e14
zilongshanren <guanghui8827@126.com>
2017-06-06 11:23:32 +0800

    fix github browser file

--- init.el
+++ init.el
@@ -92,7 +92,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(sicp)
+   dotspacemacs-additional-packages '(sicp github-browse-file)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    dotspacemacs-excluded-packages
