e0286e3e4a31ac2caf3f5df314a7d4b0d552faeb
zilongshanren <guanghui8827@126.com>
2017-07-25 17:30:06 +0800

    update config

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ahs-case-fold-search nil t)
+ '(ahs-case-fold-search nil)
  '(command-log-mode-window-size 50)
  '(company-dabbrev-minimum-length 3)
  '(company-dabbrev-other-buffers nil)
--- layers/zilongshanren-org/packages.el
+++ layers/zilongshanren-org/packages.el
@@ -459,7 +459,6 @@ holding contextual information."
 (defun zilongshanren-org/post-init-deft ()
   (progn
     (setq deft-use-filter-string-for-filename t)
-    (spacemacs/set-leader-keys-for-major-mode 'deft-mode "q" 'quit-window)
     (setq deft-recursive t)
     (setq deft-extension "org")
     (setq deft-directory deft-dir)))
