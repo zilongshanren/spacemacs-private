81b0b947f7183e646c60380c02f9b7b632fc91a1
lionqu <lionqu@tencent.com>
2018-11-05 12:05:42 +0800

    fix flycheck error display

--- layers/zilongshanren-ui/packages.el
+++ layers/zilongshanren-ui/packages.el
@@ -46,22 +46,26 @@
 
   (setq my-flycheck-mode-line
         '(:eval
-          (pcase flycheck-last-status-change
-            ((\` not-checked) nil)
-            ((\` no-checker) (propertize " -" 'face 'warning))
-            ((\` running) (propertize " ✷" 'face 'success))
-            ((\` errored) (propertize " !" 'face 'error))
-            ((\` finished)
-             (let* ((error-counts (flycheck-count-errors flycheck-current-errors))
-                    (no-errors (cdr (assq 'error error-counts)))
-                    (no-warnings (cdr (assq 'warning error-counts)))
-                    (face (cond (no-errors 'error)
-                                (no-warnings 'warning)
-                                (t 'success))))
-               (propertize (format "[%s/%s]" (or no-errors 0) (or no-warnings 0))
-                           'face face)))
-            ((\` interrupted) " -")
-            ((\` suspicious) '(propertize " ?" 'face 'warning)))))
+          (when
+              (and (bound-and-true-p flycheck-mode)
+                   (or flycheck-current-errors
+                       (eq 'running flycheck-last-status-change)))
+            (pcase flycheck-last-status-change
+              ((\` not-checked) nil)
+              ((\` no-checker) (propertize " -" 'face 'warning))
+              ((\` running) (propertize " ✷" 'face 'success))
+              ((\` errored) (propertize " !" 'face 'error))
+              ((\` finished)
+               (let* ((error-counts (flycheck-count-errors flycheck-current-errors))
+                      (no-errors (cdr (assq 'error error-counts)))
+                      (no-warnings (cdr (assq 'warning error-counts)))
+                      (face (cond (no-errors 'error)
+                                  (no-warnings 'warning)
+                                  (t 'success))))
+                 (propertize (format "[%s/%s]" (or no-errors 0) (or no-warnings 0))
+                             'face face)))
+              ((\` interrupted) " -")
+              ((\` suspicious) '(propertize " ?" 'face 'warning))))))
 
   (setq-default mode-line-misc-info
                 (assq-delete-all 'which-func-mode mode-line-misc-info))
