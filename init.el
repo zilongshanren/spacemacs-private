b7c737e588a1d0cd1c2ba7243098caabb6bf2a18
lionqu <lionqu@tencent.com>
2019-05-10 10:46:04 +0800

    add ssh-agency

--- init.el
+++ init.el
@@ -98,7 +98,7 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(sicp)
+   dotspacemacs-additional-packages '(sicp ssh-agency)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
