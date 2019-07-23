53c26095f015b0c70df14cecb97af9778235f72e
zilongshanren <guanghui8827@126.com>
2019-07-23 08:23:45 +0800

    eudic error

--- layers/zilongshanren-misc/packages.el
+++ layers/zilongshanren-misc/packages.el
@@ -1173,8 +1173,6 @@ Search for a search tool in the order provided by `dotspacemacs-search-tools'."
         (add-to-list 'magit-no-confirm 'stage-all-changes)
         (define-key magit-log-mode-map (kbd "W") 'magit-copy-section-value)
         (define-key magit-status-mode-map (kbd "s-1") 'magit-jump-to-unstaged)
-        ;; fix for emacs 27 bugs
-        (define-key magit-status-mode-map (kbd "s-c") 'magit-jump-to-unstaged)
         (define-key magit-status-mode-map (kbd "s-2") 'magit-jump-to-untracked)
         (define-key magit-status-mode-map (kbd "s-3") 'magit-jump-to-staged)
         (define-key magit-status-mode-map (kbd "s-4") 'magit-jump-to-stashes)
